388532d0f9a9bc6affcb62dbaf5ff7034c5a29ea
AndreasLagoni <46136193+AndreasLagoni@users.noreply.github.com>
2019-05-28 16:43:31 +0200

    Update admin.js

--- admin.js
+++ admin.js
@@ -8,20 +8,24 @@ function addMarker() {
   let name = document.getElementById("inputName").value;
   let date = document.getElementById("inputDate").value;
   let coords = document.getElementById("inputCoords").value;
-  let coordSplit = coords.split(",");
-  let divMaker =
-    `<div class="contentBox">
+  if (name.length != 0 && date.length != 0 && coords.length != 0) {
+    let coordSplit = coords.split(",");
+    let divMaker =
+      `<div class="contentBox">
   <h1>` +
-    name +
-    `</h1>
+      name +
+      `</h1>
   <h3>` +
-    date +
-    `<h3>
+      date +
+      `<h3>
   <a href="profilsideFRA.html" target="_blank">Læs mere</a>
 </div>`;
-  var newMarker = [coordSplit[0], coordSplit[1], divMaker, name];
-  sessionStorage.setItem(
-    "Navn: " + JSON.stringify(newMarker[3]),
-    JSON.stringify(newMarker)
-  );
+    var newMarker = [coordSplit[0], coordSplit[1], divMaker, name];
+    sessionStorage.setItem(
+      "Navn: " + JSON.stringify(newMarker[3]),
+      JSON.stringify(newMarker)
+    );
+  } else {
+    console.log("Udfyld alle felterne");
+  }
 }
